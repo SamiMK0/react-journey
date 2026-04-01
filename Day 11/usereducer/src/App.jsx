36803fa9de533f6be7e239ef90016de11a9736f5
SamiMK0 <makiyesami@gmail.com>
2026-04-01 12:20:41 +0300

    Deep dive into useReducer: understood state management using reducer logic and implemented a counter with multiple actions

---
+++ Day 11/usereducer/src/App.jsx
@@ -0,0 +1,33 @@
+import { useReducer } from "react";
+
+const initialState = {count:0};
+
+const reducer = (state,action) => {
+  switch(action.type){
+    case "increment":
+      return {...state , count:state.count+1};
+    case "decrement":
+      return {...state , count:state.count-1};
+    case "reset":
+      return {...state , count:0};
+    default: 
+    return state;
+  }
+}
+
+const App = () => {
+
+  const [state , dispatch] = useReducer(reducer , initialState );
+  // ليش ما منستعمل useState ?
+  // المشكلة لما يصير ال logic معقد و في اكثر من state
+  // الفكرة انك بتعبت امر الي هوي ال action و ال redcuer بيقرر
+  return(
+    <div>
+      <button onClick={()=> dispatch({type:"increment"})} >+</button>
+      <button onClick={()=> dispatch({type:"decrement"})} >-</button>
+      <button onClick={()=> dispatch({type:"reset"})} >Reset</button>
+      <h1>Count: {state.count}</h1>
+    </div>
+  )
+}
+export default App;
